8c705052e63a0c13bc935ceb5f5e177361d1eeac
johnfitz3 <120284650+johnfitz3@users.noreply.github.com>
2023-05-04 19:31:16 -0400

    debugging

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,30 +1,24 @@
-const fs = require('fs');
-const express = require('express');
-const router = express.Router()
+const router = require('express').Router();
 const path = require('path');
-  
-const { notes } = require('../../db/notes');
 
+const { notes } = require('./db/db.js');
 
-
-
-//Routes
-app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, './public/index.html'));
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-app.get('/notes', (req, res) => {
-res.sendFile(path.join(__dirname, './public/notes.html'));
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.get('/api/notes', (req, res) => {
-res.json(notes);
+router.get('/api/notes', (req, res) => {
+  res.json(notes);
 });
 
-app.post('/api/notes', (req, res) => {
-req.body.id = generateUniqueId();
-const note = createNewNote(req.body, notes);
-res.json(note);
+router.post('/api/notes', (req, res) => {
+  req.body.id = generateUniqueId();
+  const note = createNewNote(req.body, notes);
+  res.json(note);
 });
 
-    module.exports = router;
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,26 +1,18 @@
-
 const express = require('express');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
-
-
 const apiRoutes = require('./routes/apiRoutes');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public', { extensions: ['html', 'md'] }));
 
 app.use('/api', apiRoutes);
 
-
-
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`API server now on port ${PORT}!`);
 });
-
-
 
 
 
